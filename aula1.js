0b942161a0612227abc5b84bb6ec7c0c08e288a2
João Victor <156962782+jvictordev7@users.noreply.github.com>
2025-02-27 22:50:52 -0300

    Criei o arquivo .ts e transfirmei ele em javascript.

---
+++ aula1.js
@@ -0,0 +1,3 @@
+var nome = "João";
+var idade = 27;
+console.log("Meu nome \u00E9 ".concat(nome, " e tenho ").concat(idade, " anos."));
---
+++ aula1.ts
@@ -0,0 +1,6 @@
+
+
+const nome = "João";
+const idade = 27;
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
